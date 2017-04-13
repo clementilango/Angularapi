f67b392b5e68678c17d45ba139e778d7e64fe414
clementilango <iclementpaul@gmail.com>
2017-04-14 00:12:38 +0530

    latest checkin

--- AngularWebApi/Controllers/CategoriesController.cs
+++ AngularWebApi/Controllers/CategoriesController.cs
@@ -1,16 +1,11 @@
 ﻿using AngularWebApi;
-using ForumWebApi.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace ForumWebApi.Controllers
 {
-    //[Authorize]
+
     public class CategoriesController : ApiController
     {
         
--- AngularWebApi/Controllers/TitleController.cs
+++ AngularWebApi/Controllers/TitleController.cs
@@ -1,10 +1,7 @@
 ﻿using AngularWebApi;
-using ForumWebApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace ForumWebApi.Controllers
@@ -22,13 +19,13 @@ namespace ForumWebApi.Controllers
                 {
                     var xx = (from em in context.Topics
                               where em.CategoryId == CategoryId
-                              select new { em.TopicId, em.Title, em.Description,em.Comments,em.Created }).ToList();
+                              select new { em.TopicId, em.Title, em.Description, em.Comments, em.Created }).ToList();
                     foreach (var Topic in xx)
                     {
                         var topic = new Models.Topic();
                         topic.CategoryId = CategoryId;
                         topic.TopicId = Topic.TopicId;
-                        topic.TitleName=Topic.Title;
+                        topic.TitleName = Topic.Title;
                         topic.Description = Topic.Description;
                         topic.CommentCount = Topic.Comments.Count();
                         topic.CreatedDate = Topic.Created;
@@ -38,8 +35,30 @@ namespace ForumWebApi.Controllers
             }
             return topics;
         }
+        public Models.Topic GetTopic(int TitleId)
+        {
+            var topics = new Models.Topic();
+            using (var context = new FORUMEntities3())
+            {
+                if (context.Topics.Count() > 0)
+                {
+                    var xx = (from em in context.Topics
+                              where em.TopicId == TitleId
+                              select new { em.TopicId, em.Title, em.Description, em.Comments, em.CategoryId }).ToList();
+                    foreach (var Topic in xx)
+                    {
+                        topics.CategoryId = Topic.CategoryId;
+                        topics.TopicId = Topic.TopicId;
+                        topics.TitleName = Topic.Title;
+                        topics.Description = Topic.Description;
+                    }
+                }
+            }
+            return topics;
+        }
 
         // POST api/values
+        [HttpPost]
         public string Post(Models.Topic topic)
         {
             using (FORUMEntities3 context = new FORUMEntities3())
@@ -56,21 +75,49 @@ namespace ForumWebApi.Controllers
         }
 
         // PUT api/values/5
+        [HttpPut]
         public string Put(Models.Topic topic)
         {
             using (FORUMEntities3 context = new FORUMEntities3())
             {
-                var _topic = context.Topics.Single(t=>t.TopicId==topic.TopicId);
+                var _topic = context.Topics.Single(t => t.TopicId == topic.TopicId);
                 _topic.Title = topic.TitleName;
                 _topic.Description = topic.Description;
+                _topic.CategoryId = topic.CategoryId;
                 context.SaveChanges();
             }
-            return "Title added successfully";
+            return "Title updated successfully";
         }
 
         // DELETE api/values/5
-        public void Delete(int TopicId)
+        [HttpDelete]
+        [Route("api/Title/{TopicId}")]
+        public string Delete(int TopicId)
         {
+            using (FORUMEntities3 context = new FORUMEntities3())
+            {
+                //    var _topic = context.Topics.Single(t => t.TopicId == topic.TopicId);
+                //    _topic.Title = topic.TitleName;
+                //    _topic.Description = topic.Description;
+                //    context.
+                var _topics = from t in context.Topics
+                              where t.TopicId == TopicId
+                              select t;
+                foreach (var topic in _topics)
+                {
+                    context.Topics.Remove(topic);
+                }
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    return "An error occured while deleting the topic";
+                }
+            }
+
+            return "Title deleted successfully";
 
         }
     }
